c1ff505b55b936b4d328f31fb62441400ac86645
ronaldbmz <tiffanyelle@Tiffanys-iMac.local>
2021-04-25 18:47:16 +0200

    adding SVG wrapper in js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -11,3 +11,11 @@ var margin = {
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select(".bubblechart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
